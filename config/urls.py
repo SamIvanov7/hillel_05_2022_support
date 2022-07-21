53e38caf976a0914a706f7f594d1face7eaf7102
Sam <sam1206morfey@gmail.com>
2022-07-21 14:43:53 +0300

    homework 12 update

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from config.api import history, home, btc_usd
+from config.api import btc_usd, history, home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("home/", home),
     path("btc_usd/", btc_usd),
     path("history/", history),
