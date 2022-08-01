bd5390249ec8a7464528087e57ef2800f2b2470a
Sam <sam1206morfey@gmail.com>
2022-08-01 15:28:51 +0300

    role added

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Role
 
 # admin.site.register(User)
 
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    exclude = ["user_permissions", "groups"]
+    readonly_fields = ["password", "last_login"]
+    
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
     exclude = ["user_permissions", "groups"]
     readonly_fields = ["password", "last_login"]
