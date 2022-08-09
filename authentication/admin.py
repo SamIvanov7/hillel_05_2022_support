d5f2b774728871c18f5ac45a9f0e95a3435dcab2
Sam <sam1206morfey@gmail.com>
2022-08-09 20:52:57 +0300

    super user payload corrected

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 
 from .models import Role, User
 
-# admin.site.register(User)
-
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     exclude = ["user_permissions", "groups"]
     readonly_fields = ["password", "last_login"]
+    list_display = ["id", "username", "first_name", "last_name", "phone", "age", "role", "created_at", "updated_at"]
     list_filter = ["age"]
 
 
