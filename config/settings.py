6c43f9b62c28f36735a3e73c8ae2ce48f9269ceb
Sam <sam1206morfey@gmail.com>
2022-08-19 13:00:12 +0300

    JWTauthentication defined

--- config/settings.py
+++ config/settings.py
@@ -113,8 +113,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "authentication.backend.JWTAuthentication",
     ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
